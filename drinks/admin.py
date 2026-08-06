994a0779248c882d3f1cf9300f246dfa72249b56
Nana Kwaku <nksarps@gmail.com>
2026-08-06 11:00:32 +0000

    feat(drinks): register Drink model in admin
    Show name and description columns for Drink in the admin list view.

--- drinks/admin.py
+++ drinks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Drink
 
-# Register your models here.
+class DrinkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+admin.site.register(Drink, DrinkAdmin)
